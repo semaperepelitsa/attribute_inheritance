575868fbe56bd089b1f52339813b9427b66c31ae
Semyon Perepelitsa <sema@sema.in>
2011-12-02 23:03:20 +0800

    Rename association to accessor

--- lib/attribute_inheritance/model_additions.rb
+++ lib/attribute_inheritance/model_additions.rb
@@ -2,14 +2,14 @@ require "active_support/core_ext/module/aliasing"
 
 module AttributeInheritance
   module ModelAdditions
-    def attr_inherited(association, *attributes)
-      define_method :"#{association}_with_setting_defaults=" do |object|
+    def attr_inherited(accessor, *attributes)
+      define_method :"#{accessor}_with_setting_defaults=" do |object|
         attributes.each do |attr|
           send(attr) || send("#{attr}=", object.send(attr))
         end if object
-        send(:"#{association}_without_setting_defaults=", object)
+        send(:"#{accessor}_without_setting_defaults=", object)
       end
-      alias_method_chain :"#{association}=", :setting_defaults
+      alias_method_chain :"#{accessor}=", :setting_defaults
     end
   end
 end
