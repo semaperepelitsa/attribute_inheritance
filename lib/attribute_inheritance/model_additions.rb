3411873525cb2b29981cbee53192b185ccbac9a0
Semyon Perepelitsa <sema@sema.in>
2011-12-02 23:02:18 +0800

    Use just "send" because accessor can be private as well

--- lib/attribute_inheritance/model_additions.rb
+++ lib/attribute_inheritance/model_additions.rb
@@ -5,9 +5,9 @@ module AttributeInheritance
     def attr_inherited(association, *attributes)
       define_method :"#{association}_with_setting_defaults=" do |object|
         attributes.each do |attr|
-          public_send(attr) || public_send("#{attr}=", object.public_send(attr))
+          send(attr) || send("#{attr}=", object.send(attr))
         end if object
-        public_send(:"#{association}_without_setting_defaults=", object)
+        send(:"#{association}_without_setting_defaults=", object)
       end
       alias_method_chain :"#{association}=", :setting_defaults
     end
